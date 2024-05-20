7c16f1c4eae5b109f107e89df9df834098cd78c5
petinggint <155607304+petinggint@users.noreply.github.com>
2024-05-20 23:24:23 +0700

    FIKS filters.py

--- MukeshRobot/modules/helper_funcs/filters.py
+++ MukeshRobot/modules/helper_funcs/filters.py
@@ -7,19 +7,19 @@ from MukeshRobot import DEMONS, DEV_USERS, DRAGONS
 class CustomFilters(object):
     class _Supporters(MessageFilter):
         def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in 7054610436)
+            return bool(message.from_user and message.from_user.id in 484576504)
 
     support_filter = _Supporters()
 
     class Sudoers(MessageFilter):
         def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in 7054610436)
+            return bool(message.from_user and message.from_user.id in 484576504)
 
     sudo_filter = Sudoers()
 
     class Developers(MessageFilter):
         def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in 7054610436)
+            return bool(message.from_user and message.from_user.id in 484576504)
 
     dev_filter = Developers()
 
